651e519cd7528a1c029382663884ec50cf8ae5c4
adriansebastiann <79217003+adriansebastiann@users.noreply.github.com>
2022-03-18 20:12:42 +0200

    Compulsory ready

---
+++ Laborator4/src/compulsory/Intersection.java
@@ -0,0 +1,13 @@
+package compulsory;
+
+public class Intersection {
+    String name;
+
+    public Intersection(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
